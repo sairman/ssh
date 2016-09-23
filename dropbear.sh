d6edd3cbc816a2ea076ac15b25bd2c6e650c6df2
sairman <sairman1996namrias@gmail.com>
2016-09-24 00:21:21 +0800

    Update dropbear.sh

--- dropbear.sh
+++ dropbear.sh
@@ -1,4 +1,4 @@
-apt-get -y install dropbear
+apt-get install -y dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 443 -p 80"/g' /etc/default/dropbear
