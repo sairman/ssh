8b19e3c29200abf8edc28da6302431506cc7b23d
sairman <sairman1996namrias@gmail.com>
2016-09-23 13:51:14 +0800

    Update squid3.sh

--- squid3.sh
+++ squid3.sh
@@ -1,4 +1,5 @@
 apt-get -y install squid3
+mv squid.conf squid.conf.bak
 wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/sairman/ssh/master/squid.conf"
 sed -i $MYIP2 /etc/squid3/squid.conf;
 service squid3 restart
