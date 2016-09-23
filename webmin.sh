4ab582c9677d090e8274ccde185012cf3fc71244
sairman <sairman1996namrias@gmail.com>
2016-09-23 14:09:37 +0800

    Create webmin.sh

---
+++ webmin.sh
@@ -0,0 +1,4 @@
+# instalasi webmin
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.690_all.deb && dpkg -i webmin_1.690_all.deb
+apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+apt-get -f install -y
