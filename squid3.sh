607007009382b0bf84ec5a40a6a01411b79fead9
sairman <sairman1996namrias@gmail.com>
2016-09-24 19:29:39 +0800

    Update squid3.sh

--- squid3.sh
+++ squid3.sh
@@ -2,3 +2,4 @@ apt-get install -y squid3
 mv squid.conf squid.conf.bak
 curl https://raw.githubusercontent.com/sairman/ssh/master/squid.conf > /etc/squid3/squid.conf
 service squid3 restart
+nano /etc/squid3/squid.conf
