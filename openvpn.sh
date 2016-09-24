1f980dd908d954c303f6dbc5cad8dcb0eda6bd19
sairman <sairman1996namrias@gmail.com>
2016-09-24 17:41:44 +0800

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -26,5 +26,5 @@ chmod +x /etc/network/if-up.d/iptables
 iptables -t nat -I POSTROUTING -s 10.9.0.0/24 -o eth0 -j MASQUERADE
 echo "iptables -t nat -I POSTROUTING -s 10.9.0.0/24 -o eth0 -j MASQUERADE" > /etc/iptables9.conf
 iptables-save > /etc/iptables9.conf
-nano /etc/openvpn/easy-rsa/2.0/keys/ca.crt
 service openvpn restart
+nano /etc/openvpn/easy-rsa/2.0/keys/ca.crt
