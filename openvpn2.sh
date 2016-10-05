813193f084acff687a6e480f600aff8af38244e3
sairman <sairman1996namrias@gmail.com>
2016-10-05 20:41:36 +0800

    Create openvpn2.sh

---
+++ openvpn2.sh
@@ -0,0 +1,8 @@
+cd /etc/openvpn
+curl https://raw.githubusercontent.com/sairman/ssh/master/server2.conf > server2.conf
+iptables -t nat -I POSTROUTING -s 10.7.0.0/24 -o eth0 -j MASQUERADE
+echo "iptables -t nat -I POSTROUTING -s 10.7.0.0/24 -o eth0 -j MASQUERADE" > /etc/iptables7.conf
+iptables-save > /etc/iptables7.conf
+curl https://raw.githubusercontent.com/sairman/ssh/master/iptables.conf > /etc/network/if-up.d/iptables
+chmod +x /etc/network/if-up.d/iptables
+service openvpn restart
