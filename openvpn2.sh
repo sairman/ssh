4431887c523b712b3567b30bc8512ad962dc690c
sairman <sairman1996namrias@gmail.com>
2016-10-05 21:14:06 +0800

    Update openvpn2.sh

--- openvpn2.sh
+++ openvpn2.sh
@@ -1,4 +1,5 @@
 cd /etc/openvpn
+service vpnserver stop
 curl https://raw.githubusercontent.com/sairman/ssh/master/server2.conf > server2.conf
 iptables -t nat -I POSTROUTING -s 10.7.0.0/24 -o eth0 -j MASQUERADE
 echo "iptables -t nat -I POSTROUTING -s 10.7.0.0/24 -o eth0 -j MASQUERADE" > /etc/iptables7.conf
