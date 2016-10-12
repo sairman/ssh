249b15e8747eda647c450ccd369660f16cda9fa8
sairman <sairman1996namrias@gmail.com>
2016-10-12 21:51:10 +0800

    Update ovpn1.sh

--- ovpn1.sh
+++ ovpn1.sh
@@ -1,6 +1,1 @@
-apt-get install -y openvpn
-cp -a /usr/share/doc/openvpn/examples/easy-rsa /etc/openvpn/
-cd /etc/openvpn/easy-rsa/2.0
-source ./vars
-./clean-all
-./build-ca
+apt-get install -y openvpn && cp -a /usr/share/doc/openvpn/examples/easy-rsa /etc/openvpn/ && cd /etc/openvpn/easy-rsa/2.0 && source ./vars && ./clean-all && ./build-ca
