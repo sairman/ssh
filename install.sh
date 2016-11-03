8ab5650914e7ad2aec118c64bf38696c9022fc13
sairman <sairman1996namrias@gmail.com>
2016-11-03 22:45:41 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+apt-get update
+apt-get upgrade -y
+apt-get install nano -y
+apt-get install curl -y
+apt-get install openvpn -y
+apt-get install -y dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 443 -p 80"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+service dropbear restart
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.690_all.deb && dpkg -i webmin_1.690_all.deb
+apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+apt-get -f install -y
+sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+service webmin restart
+apt-get install -y squid3
+mv squid.conf squid.conf.bak
+curl https://raw.githubusercontent.com/sairman/ssh/master/squid.conf > /etc/squid3/squid.conf
+nano /etc/squid3/squid.conf
