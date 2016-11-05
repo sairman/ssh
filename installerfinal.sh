2b0383eda9ad2c60dbfd48dee77ae33fcb65d022
sairman <sairman1996namrias@gmail.com>
2016-11-05 15:45:24 +0800

    Update installerfinal.sh

--- installerfinal.sh
+++ installerfinal.sh
@@ -9,3 +9,7 @@ chmod +x installerwebserver.sh
 sh installerwebserver.sh
 cp /etc/openvpn/clientconfig/client-udp.ovpn /home/vps/public_html
 cp /etc/openvpn/clientconfig/client-tcp.ovpn /home/vps/public_html
+cd /usr/bin
+curl https://raw.githubusercontent.com/sairman/ssh/master/cek-user.sh > cek
+chmod +x cek
+./cek
