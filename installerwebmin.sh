7f90aef63fd749ba91e3188752df9c5102deaaae
sairman <sairman1996namrias@gmail.com>
2016-11-05 16:12:00 +0800

    Update installerwebmin.sh

--- installerwebmin.sh
+++ installerwebmin.sh
@@ -4,6 +4,7 @@ apt-get install nano -y
 apt-get install curl -y
 wget -qO- ipv4.icanhazip.com
 IP=$(wget -qO- ipv4.icanhazip.com)
+apt-get install dropbear -y
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 443 -p 80"/g' /etc/default/dropbear
