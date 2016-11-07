5c3943b16d7e67e0101b9228ba85422ce23f3211
sairman <sairman1996namrias@gmail.com>
2016-11-07 23:16:18 +0800

    Update installerwebmin.sh

--- installerwebmin.sh
+++ installerwebmin.sh
@@ -7,8 +7,9 @@ IP=$(wget -qO- ipv4.icanhazip.com)
 apt-get install dropbear -y
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 443 -p 80"/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 443"/g' /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
+nano /etc/default/dropbear
 wget http://prdownloads.sourceforge.net/webadmin/webmin_1.690_all.deb && dpkg -i webmin_1.690_all.deb
 apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 apt-get -f install -y
