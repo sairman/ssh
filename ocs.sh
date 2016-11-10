82afa4a031a9df35383a637b35eb711a943bcdff
sairman <sairman1996namrias@gmail.com>
2016-11-10 22:52:24 +0800

    Update ocs.sh

--- ocs.sh
+++ ocs.sh
@@ -6,7 +6,7 @@ apt-get install nginx php5 php5-fpm php5-cli php5-mysql php5-mcrypt
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
-curl https://github.com/sairman/conf/blob/master/nginx.conf > /etc/nginx/nginx.conf
+curl https://raw.githubusercontent.com/sairman/conf/master/nginx.conf > /etc/nginx/nginx.conf
 curl https://raw.githubusercontent.com/sairman/conf/master/vps.conf > /etc/nginx/conf.d/vps.conf
 sed -i 's/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
@@ -23,4 +23,3 @@ git init
 git remote add origin https://github.com/sairman/ocs-panel.git
 git pull origin master
 mysql -u root -p
-CREATE DATABASE IF NOT EXISTS OCS_PANEL;EXIT;
