bd19bcf436fe2eec9bef055d8eda3edb783928ec
sairman <sairman1996namrias@gmail.com>
2016-11-12 15:58:38 +0800

    Update fitur.sh

--- fitur.sh
+++ fitur.sh
@@ -1,2 +1,9 @@
+IP=$(wget -qO- ipv4.icanhazip.com)
 echo -e "Created By VPS TERMURAH"
-echo -e "webmin http:://$IP:10000"
+echo -e ""
+echo -e "webmin http://$IP:10000"
+echo -e "Open Vpn UDP : http://$IP:81/client-udp.ovpn"
+echo -e "Open Vpn UDP : http://$IP:81/client-tcp.ovpn"
+echo -e "Akun trial ssh/open vpn 30 menit ketik : trial"
+echo -e "Cek user login ssh ketik : cek"
+echo -e "Squid port : 80, 8080, 8000 & 3128"
