b359b9608dcc154098dac55e318743c9f41032d3
sairman <sairman1996namrias@gmail.com>
2017-04-27 18:10:12 +0800

    Update openvpn-axishitschat.sh

--- openvpn-axishitschat.sh
+++ openvpn-axishitschat.sh
@@ -1,5 +1,5 @@
-wget -qO- ipv4.icanhazip.com
-IP=$(wget -qO- ipv4.icanhazip.com)
+curl -s https://4.ifcfg.me
+IP=$(curl -s https://4.ifcfg.me)
 curl https://raw.githubusercontent.com/sairman/ssh/master/hits-chat.ovpn > /etc/openvpn/clientconfig/hits-chat.ovpn
 sed -i "s|remote ip 55|remote $IP 55|" /etc/openvpn/clientconfig/hits-chat.ovpn
 sed -i "s|http-proxy ip 3128|http-proxy $IP 3128|" /etc/openvpn/clientconfig/hits-chat.ovpn
